d7ca94b3662cc941ea086dfe1d1dbd419a20fd64
flavienxgrbld <flavien.garibaldi@gmail.com>
2025-10-21 20:24:54 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// config.php
+// Mettre ici les infos réelles : IP_SERVEUR_BDD, DB_USER, DB_PASS
+define('DB_HOST', '192.168.1.55');   // ex: '192.168.1.10'
+define('DB_USER', 'root');
+define('DB_PASS', 'V@%y!2Nvqx&2t7');
+define('DB_NAME', 'brinks');
+
+// Options session
+session_start();
+ini_set('session.cookie_httponly', 1);
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    ini_set('session.cookie_secure', 1);
+}
