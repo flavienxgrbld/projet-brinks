b08c3966207c6630ae3db29e3ad9d9651dd4c4e7
flavienxgrbld <flavien.garibaldi@gmail.com>
2025-10-22 16:07:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,12 +21,9 @@ $role = $_SESSION['role'] ?? 'user';
       <p><a href="admin_create.php">Créer un nouvel utilisateur</a></p>
       <p><a href="admin_users.php">Gérer utilisateurs</a></p>
       <p><a href="admin_logs.php">Voir journal admin</a></p>
-    <?php endif; ?>
-    <?php if ($_SESSION['role'] === 'admin'): ?>
       <p><a href="admin_temps.php">Gestion du temps</a></p>
-    <?php else: ?>
-      <p><a href="mes_heures.php">Mes heures</a></p>
     <?php endif; ?>
+    <p><a href="mes_heures.php">Mes heures</a></p>
     <p><a href="logout.php">Se déconnecter</a></p>
   </div>
 </body>
